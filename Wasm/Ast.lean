0753360c3bd96fc2b6fcf95aa09772e277dff8a6
Mikail Khan <mikail@mikail-khan.com>
2025-08-19 22:00:41 -0400

    proved some stuff

--- Wasm/Ast.lean
+++ Wasm/Ast.lean
@@ -1,13 +1,19 @@
+import Aesop
+
 inductive ValType : Type
   | I32
+  deriving Repr
 
-inductive Val where
+inductive Val : Type
   | I32 (n : UInt32)
+  deriving Repr
 
 def Val.toValType : Val -> ValType
   | I32 _ => ValType.I32
 
 def mapTypes : List Val -> List ValType := List.map Val.toValType
+
+theorem map_type_head : mapTypes (h :: xs) = h.toValType :: mapTypes xs := by rfl
 
 def ValType.toType : ValType -> Type
 | ValType.I32 => UInt32
@@ -22,13 +28,22 @@ inductive Prog : List ValType -> List ValType -> Type where
 
 infixr:50 " ;; " => Prog.cons
 
-def testProg : Prog [] [I32] := 
+def testProg : Prog [] [ValType.I32] := 
   Op.I32Const 42 ;; 
   Op.I32Const 52 ;; 
   Op.I32Add ;;
   Prog.nil
 
-def evalOp (op : Op inp out) (st : List Val) (h : mapTypes st = inp) : List Val := match (op, st) with
-| (Op.I32Const n, _) => Val.I32 n :: st
-| (Op.I32Add, Val.I32 a :: Val.I32 b :: xs) => Val.I32 (a + b) :: xs
-| (Op.I32Add, s) => False.elim (by sorry)
+#check List.map_cons
+def evalOp (inp : List Val) (op : Op (mapTypes inp) out) : {result : List Val // mapTypes result = out} := match inp, op with
+| Val.I32 a :: Val.I32 b :: rest, Op.I32Add => 
+  ⟨Val.I32 (a + b) :: rest, by rw [Val.toValType]; unfold mapTypes; rw [List.map_cons]⟩
+| inp, Op.I32Const n => ⟨Val.I32 n :: inp, by unfold mapTypes; rw [List.map_cons]⟩
+
+def evalProg (inp : List Val) (prog : Prog (mapTypes inp) out) : {result : List Val // mapTypes result = out} := match prog with
+| Prog.nil => ⟨inp, by rfl⟩
+| Prog.cons op rest =>
+    let ⟨step, h1⟩ := evalOp inp op
+    evalProg step (h1 ▸ rest)
+
+#eval (evalProg [] testProg).val
