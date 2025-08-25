632eeb7e0adb4fe136b3db01e1ce0debf042c8d3
Mikail Khan <mikail@mikail-khan.com>
2025-08-24 23:55:05 -0400

    cleanup

--- Wasm/Compiler.lean
+++ Wasm/Compiler.lean
@@ -7,30 +7,19 @@ inductive Expr : ValType -> Type
 | I32Sub (lhs : Expr .I32) (rhs : Expr .I32) : Expr .I32
 | F32Add (lhs : Expr .F32) (rhs : Expr .F32) : Expr .F32
 
-def Expr.eval' (e : Expr T) : TVal T := match e with
-| .I32Lit n => .I32 n
-| .F32Lit n => .F32 n
-| .I32Add lhs rhs => .I32 $ lhs.eval'.toRepr + rhs.eval'.toRepr
-| .I32Sub lhs rhs => .I32 $ lhs.eval'.toRepr - rhs.eval'.toRepr
-| .F32Add lhs rhs => .F32 $ lhs.eval'.toRepr + rhs.eval'.toRepr
-
 theorem val_repr (v : { v : Val // v.toValType = T}) : v.val.toValType.repr = T.repr := by rw [v.property];
 
-def Expr.eval'' (e : Expr T) : { v : Val // v.toValType = T } := match e with
+def Expr.eval (e : Expr T) : { v : Val // v.toValType = T } := match e with
 | .I32Lit n => ⟨.I32 n, rfl⟩
 | .F32Lit n => ⟨.F32 n, rfl⟩
-| .I32Add lhs rhs => ⟨.I32 $ Val.toReprR lhs.eval'' + Val.toReprR rhs.eval'', rfl⟩
-| .I32Sub lhs rhs => ⟨.I32 $ Val.toReprR lhs.eval'' - Val.toReprR rhs.eval'', rfl⟩
-| .F32Add lhs rhs => ⟨.F32 $ Val.toReprR lhs.eval'' + Val.toReprR rhs.eval'', rfl⟩
+| .I32Add lhs rhs => ⟨.I32 $ Val.toReprR lhs.eval + Val.toReprR rhs.eval, rfl⟩
+| .I32Sub lhs rhs => ⟨.I32 $ Val.toReprR lhs.eval - Val.toReprR rhs.eval, rfl⟩
+| .F32Add lhs rhs => ⟨.F32 $ Val.toReprR lhs.eval + Val.toReprR rhs.eval, rfl⟩
 
-def Expr.eval (e : Expr T) : T.repr := 
-  let tv := e.eval'
-  let ⟨v, h⟩ := tv.toValR
-  have h' : v.toValType.repr = T.repr := by rw [h];
-  h ▸ v.toRepr
+theorem eval_t {e : Expr T} : e.eval.val.toValType = T := by grind
 
 def testExpr : Expr .I32 := .I32Add (.I32Lit 5) (.I32Add (.I32Lit 10) (.I32Lit 15))
-def evalRes := testExpr.eval
+def evalRes := testExpr.eval.val
 #eval evalRes
 
 def Expr.compile (e : Expr T) : Prog inp (T::inp) := match e with
@@ -44,39 +33,9 @@ def compiledExpr (inp : StackTypes := []) : Prog inp (.I32::inp) := testExpr.com
 def compileRes := evalProg [] compiledExpr
 #eval compileRes
 
-def progResHead (progRes : { result : Stack // result.types = (x::xs) }) : { val : Val // val.toValType = x } :=
-  let ⟨st, h⟩ := progRes
-  let hd::tl := st
-  have h' : hd.toValType = x := by
-    unfold Stack.types at h;
-    rw [List.map_cons, List.cons_eq_cons] at h; apply h.left;
-  ⟨hd, h'⟩
-
-def eval_eq_compile (e : Expr T) : Prop :=
-  let prog := @e.compile T []
-  let compile_res := evalProg [] prog
-  let ⟨res, h⟩ := progResHead compile_res
-  let eval_res := e.eval
-  have h' : T.repr = res.toValType.repr := by rw [h]
-  res.toRepr = (h' ▸ eval_res)
-
-def test_expr_eq : Bool :=
-  let prog := testExpr.compile
-  let compile_res := evalProg [] prog
-  let ⟨res, h⟩ := progResHead compile_res
-  have h' : UInt32 = res.toValType.repr := by rw [h]
-  let res : UInt32 := (h' ▸ res.toRepr)
-  let eval_res := testExpr.eval
-  res == eval_res
-#eval test_expr_eq
-
 @[simp]
 theorem eval_nil {st : Stack} {h : st.types = inpTys} : (evalProg' (@Prog.nil inpTys) st h).val = st := by
   unfold evalProg'; rfl
-
-/- @[simp] -/
-/- theorem prog_cast_nil {a b : StackTypes} (h : a = b) : (h ▸ (@Prog.nil a)) = (@Prog.nil b) := by -/
-/-   subst h; rfl -/
 
 @[simp]
 theorem concat_nil_l {a b : StackTypes} {lhs : Prog a b} : Prog.concat lhs Prog.nil = lhs := by
@@ -102,27 +61,25 @@ theorem eval_prog_append : evalProg' (Prog.append l op) st h = evalOp' op (evalP
     | cons l_op rest ih => simp [evalProg']; rw [ih];
 
 theorem compile_eq_eval' {T : ValType} {stypes : StackTypes} {st : Stack} {h : st.types = stypes}
-  : ∀ (e : Expr T), (evalProg' e.compile st h).val = e.eval''.val :: st
+  : ∀ (e : Expr T), (evalProg' e.compile st h).val = e.eval.val :: st
   := by
       intro e;
       induction e generalizing st stypes with
       | I32Lit n => conv => left; simp [evalProg', Expr.compile, evalOp'];
-                    conv => right; simp [Expr.eval', TVal.toVal];
-                    rfl;
+                    conv => right; simp [Expr.eval, TVal.toVal];
       | F32Lit n => conv => left; simp [evalProg', Expr.compile, evalOp'];
-                    conv => right; simp [Expr.eval', TVal.toVal];
-                    rfl;
+                    conv => right; simp [Expr.eval, TVal.toVal];
       | I32Add lhs rhs ihl ihr =>
           conv => left; unfold Expr.compile; rw [prog_append_assoc];
           rw [prog_eval_concat];
-          · conv => left; arg 1; arg 2; rw [ihl];
-            rw [eval_prog_append];
-            rotate_left;
-            · grind
-            · grind
-            · conv => left; arg 1; arg 2; rw [ihr];
-              conv => right; unfold Expr.eval'';
-              rw [eval_add] <;> grind;
+          conv => left; arg 1; arg 2; rw [ihl];
+          rw [eval_prog_append];
+          rotate_left;
+          · grind
+          · grind
+          · conv => left; arg 1; arg 2; rw [ihr];
+            conv => right; unfold Expr.eval;
+            rw [eval_add] <;> grind;
       | I32Sub lhs rhs ihl ihr =>
           conv => left; unfold Expr.compile; rw [prog_append_assoc];
           rw [prog_eval_concat];
@@ -130,9 +87,9 @@ theorem compile_eq_eval' {T : ValType} {stypes : StackTypes} {st : Stack} {h : s
             rw [eval_prog_append];
             rotate_left;
             · grind
-            · grind;
+            · grind
             · conv => left; arg 1; arg 2; rw [ihr];
-              conv => right; unfold Expr.eval'';
+              conv => right; unfold Expr.eval;
               rw [eval_sub] <;> grind;
       | F32Add lhs rhs ihl ihr =>
           conv => left; unfold Expr.compile; rw [prog_append_assoc];
@@ -143,7 +100,7 @@ theorem compile_eq_eval' {T : ValType} {stypes : StackTypes} {st : Stack} {h : s
             · grind
             · grind
             · conv => left; arg 1; arg 2; rw [ihr];
-              conv => right; unfold Expr.eval'';
+              conv => right; unfold Expr.eval;
               rw [eval_add_f32] <;> grind;
 
-theorem compile_eq_eval : ∀ (e : Expr T), (evalProg' e.compile [] h).val = [e.eval''.val] := by simp [compile_eq_eval'];
+theorem compile_eq_eval : ∀ (e : Expr T), (evalProg' e.compile [] h).val = [e.eval.val] := by simp [compile_eq_eval'];
